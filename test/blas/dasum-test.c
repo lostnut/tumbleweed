5052b684339c2da512c8125f32054235dcf610ac
lostnut <lostnut@pm.me>
2019-01-13 20:03:35 +0100

    Update dasum tests

--- test/blas/dasum-test.c
+++ test/blas/dasum-test.c
@@ -19,9 +19,39 @@ void dasum_null_length(void)
         assert_double_equals(result, 0.0);
 }
 
+void dasum_unity_increment(void)
+{
+        double x[2]   = {1.0, 2.0};
+        int n         = 2;
+        int incx      = 1;
+        double result = dasum(n, x, incx);
+        assert_double_equals(result, 3.0);
+}
+
+void dasum_positive_increment(void)
+{
+        double x[4]   = {1.0, 2.0, 3.0, 4.0};
+        int n         = 2;
+        int incx      = 2;
+        double result = dasum(n, x, incx);
+        assert_double_equals(result, 4.0);
+}
+
+void dasum_neg_incremnt(void)
+{
+        double x[2]   = {1.0, 2.0};
+        int n         = 2;
+        int incx      = -1;
+        double result = dasum(n, (x + 1), incx);
+        assert_double_equals(result, 3.0);
+}
+
 void dasum_test(void)
 {
         test(dasum_negative_length);
         test(dasum_null_length);
+        test(dasum_unity_increment);
+        test(dasum_positive_increment);
+        test(dasum_negative_length);
 }
 
